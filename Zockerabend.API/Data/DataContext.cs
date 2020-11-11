9c3c049878f291df15864e11e25d859ba215c890
Florian Stegemann <fstegemann87@googlemail.com>
2020-11-11 10:07:31 +0100

    Database setup

---
+++ Zockerabend.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Zockerabend.API.Models;
+
+namespace Zockerabend.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> User { get; set; }
+    }
+}
